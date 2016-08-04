eda284cd1401ec05c504d6c510f97ec25d73992d
BinnyGandhi <bkgandhi27296@gmail.com>
2016-08-04 10:54:27 +0530

    Change design of home page

--- homepage.js
+++ homepage.js
@@ -39,7 +39,7 @@ function loginValidation() {
                     if (result.status === '200') {
                         localStorage.setItem('token', result.message);
                         //ToDo: page redirection
-                        window.location.replace('');
+                        window.location.replace('headOffice.html');
                     }
                     else {
                         //ToDo: if result.message != '200'
